8f42a9155a7dbc9465c74b78bbbf335aa60eb779
Justin <herojfl@gmail.com>
2020-03-07 13:57:32 -0500

    Fixed Isometric Movement issue

--- Assets/Scripts/SCR_PlayerMovement.cs
+++ Assets/Scripts/SCR_PlayerMovement.cs
@@ -4,24 +4,45 @@ using UnityEngine;
 
 public class SCR_PlayerMovement : MonoBehaviour
 {
-    public float speed;
-    private CharacterController character;
+
+    [SerializeField]
+    float speed = 4f;
+    Vector3 forward, right;
     
     // Start is called before the first frame update
     void Start()
     {
-        character = GetComponent<CharacterController>();
-        Debug.Log("Teeeeeeeeeeeest");
+        //Declaring forward variables to be based off of cameras position
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0,90,0))*forward;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        //Calling move function anytime the movement keys are pressed.
+        if(Input.GetAxis("HorizontalKey") > 0 
+        || Input.GetAxis("VerticalKey") > 0 
+        || Input.GetAxis("HorizontalKey") < 0 
+        || Input.GetAxis("VerticalKey") < 0)
+            {
+                Move();
+            }
+    }
 
-        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
+    void Move()
+    {
+        //Getting different vector3 values for both the horizontal movement and vertical movement to set transform of player
+        Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"),0,Input.GetAxis("VerticalKey"));
+        Vector3 rightMovement = right * speed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        Vector3 upMovement = forward * speed * Time.deltaTime * Input.GetAxis("VerticalKey");
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
 
-        character.Move(movement * speed);
+        //Using previously declared values to update players transform and movement based off camera position.
+        transform.forward = heading;
+        transform.position += rightMovement;
+        transform.position += upMovement;
     }
 }
