63ac7d9dc04522c85b632d8e76529c95b4d50242
Edward <tavarez92@knights.ucf.edu>
2020-04-10 03:10:59 -0400

    oops, forgot to fix something
    lol

--- Assets/Scripts/Enemy Scripts/State_Machine_AI/SCR_DeathState.cs
+++ Assets/Scripts/Enemy Scripts/State_Machine_AI/SCR_DeathState.cs
@@ -7,7 +7,7 @@ public class SCR_DeathState : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Destroy(animator.gameObject, .5f);
+        Destroy(animator.gameObject);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
