248767dbde78bb02714efb502c6240ada73d329a
Justin <herojfl@gmail.com>
2020-04-20 19:32:47 -0400

    +1

--- Assets/SCR_UpgradesController.cs
+++ Assets/SCR_UpgradesController.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class SCR_UpgradesController : MonoBehaviour
 {
-    
+
+    [SerializeField]
     void Start()
     {
         
     }
 
+    public struct upgrade1Variables
+    {
+        public GameObject upgrade1Description;
+        public GameObject upgrade1Video;
+    }
+
     // Update is called once per frame
-    [SerializeField]
         void Update()
     {
         
