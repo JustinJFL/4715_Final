fa0d7be7e75879b319139da9c06aeb21e925cbfc
Justin <herojfl@gmail.com>
2020-04-26 17:01:43 -0400

    optimized level 2
    added colliders for missing floors and buildings as well as moving victory and player spawn to make more sense

--- Assets/Scripts/Game Scripts/SCR_Victory.cs
+++ Assets/Scripts/Game Scripts/SCR_Victory.cs
@@ -16,7 +16,7 @@ public class SCR_Victory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if(Input.GetKeyDown(KeyCode.Q))
+        if(Input.GetKeyDown(KeyCode.Q))
         {
                     GameObject.FindWithTag("GameController").GetComponent<SCR_GameManager>().lastLevel++;
                 GameObject.FindWithTag("HUD").GetComponent<Canvas>().enabled = false;
@@ -32,7 +32,7 @@ public class SCR_Victory : MonoBehaviour
 
                 GameObject.FindWithTag("Tutorial").GetComponent<SCR_Tutorial_Menus>().PopUpCounter();
             
-        }*/
+        }
     }
 
     void OnTriggerEnter(Collider other) 
@@ -58,8 +58,19 @@ public class SCR_Victory : MonoBehaviour
             }
             else if(SceneManager.GetActiveScene().name == "Level 2")
             {
+                GameObject.FindWithTag("GameController").GetComponent<SCR_GameManager>().lastLevel++;
                 GameObject.FindWithTag("HUD").GetComponent<Canvas>().enabled = false;
                 SceneManager.LoadScene("UpgradeShop", LoadSceneMode.Single);
+
+                DontDestroyOnLoad(GameObject.FindWithTag("GameController"));
+
+                DontDestroyOnLoad(GameObject.Find("CameraTarget"));
+
+                DontDestroyOnLoad(GameObject.FindWithTag("HUD"));
+
+                DontDestroyOnLoad(GameObject.Find("EventSystem"));
+
+                GameObject.FindWithTag("Tutorial").GetComponent<SCR_Tutorial_Menus>().PopUpCounter();
             }
         }
     }
