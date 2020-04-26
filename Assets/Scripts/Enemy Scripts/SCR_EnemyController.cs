91723986fbd84e20f2d67316a894856496182064
Justin <herojfl@gmail.com>
2020-04-25 22:54:13 -0400

    Ed fix the shrimp pls

--- Assets/Scripts/Enemy Scripts/SCR_EnemyController.cs
+++ Assets/Scripts/Enemy Scripts/SCR_EnemyController.cs
@@ -3,27 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using UnityEngine.Animations;
 
 public class SCR_EnemyController : MonoBehaviour
 {   
     public float moveSpeed = 4;
     public float knockbackForce;
-    public WanderBoundry wanderLimit;
+    public WanderBoundry wanderLimit;
+
     public bool isPlayerSpotted;
 
     private Rigidbody enemyRigidBody;
     private SCR_PlayerCombat playerAttack;
     private Vector3 knockbackDirection;
+    private int attackNum;
 
     public Scrollbar enemyHealthBar;
     //Target attached to the camera for the health bar to orient itself toward the camera.
     //public SCR_EnemySight enemySight;
     public GameObject enemyHealthBarTarget;
 
+    public Animator enemyAnimator;
+
+    public bool setToIdle = false;
+
+    private bool attack1;
+    private bool attack2;
+    private bool attack3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
         enemyRigidBody = GetComponent<Rigidbody>();
         GameObject movement = GameObject.FindWithTag("Player");
         if (movement == null)
@@ -49,6 +59,8 @@ public class SCR_EnemyController : MonoBehaviour
         Mathf.Clamp(transform.position.x, wanderLimit.xMin, wanderLimit.xMax),
         2.2f,
         Mathf.Clamp(transform.position.z, wanderLimit.zMin, wanderLimit.zMax));
+    
+        if(setToIdle) SetToIdle();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -64,19 +76,62 @@ public class SCR_EnemyController : MonoBehaviour
             }
             //Debug.Log("I GOT HIT"); 
         }
+
+
     }
     private void OnCollisionEnter(Collision collision)
 
     {
+        if(collision.gameObject.tag == "Player")
+        {
+            attackNum = Random.Range(1,4);
+            //enemyAnimator.SetInteger("AttackNum",attackNum);
+            //enemyAnimator.SetInteger("AttackNum",0);
+            //setToIdle = true;
+
+            if (attackNum == 1)
+            {
+                enemyAnimator.SetBool("Attack1",true);
+                SetToIdle();
+            }
+
+            else if (attackNum == 2)
+            {
+                enemyAnimator.SetBool("Attack2",true);
+                SetToIdle();
+            }
+
+            else if (attackNum == 3)
+            {
+                enemyAnimator.SetBool("Attack3",true);
+                SetToIdle();
+            }
+        }
 
         if(collision.gameObject.tag == "Wall")
 
         {
-
             transform.eulerAngles = new Vector3(0, Random.Range(-360, 360), 0);
-
         }
 
     }
+        void SetToIdle()
+        {
+           // attackNum = 0;
+            //enemyAnimator.SetInteger("AttackNum",0);
+            if(enemyAnimator.GetCurrentAnimatorStateInfo(0).IsName("Tail_Attack"))
+            {
+                enemyAnimator.SetBool("Attack1",false);
+            }
 
+            else if(enemyAnimator.GetCurrentAnimatorStateInfo(0).IsName("Wind_Up"))
+            {
+                enemyAnimator.SetBool("Attack2",false);
+            }
+
+            else if(enemyAnimator.GetCurrentAnimatorStateInfo(0).IsName("Claw_attack"))
+            {
+                enemyAnimator.SetBool("Attack3",false);
+            }
+        }
 }
